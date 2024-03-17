3c0424ede7eaa260358ff5052e62e57cd70e22ba
Fawzi Gharib <fawz11gharib@gmail.com>
2024-03-18 01:21:06 +0300

    Assignment Done

---
+++ src/components/NavBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function NavBar() {
+  const links = ["home", "about", "projects"];
+
+  return <nav>{links.map((link,index)=>(<a href={`#${link}`} key={index}>{link}</a>))}</nav>;
+}
+
+export default NavBar;
